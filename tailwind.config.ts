19046cc30bd79045679b7e13b2ced8db5616c814
Allan Simoyi <allansimoyi@gmail.com>
2024-01-06 12:03:12 +0200

    Add progress bar to toolbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        progress: {
+          from: { left: '-50%' },
+          to: { left: '100%' },
+        },
+      },
+      animation: {
+        progress: 'progress 1.5s ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
